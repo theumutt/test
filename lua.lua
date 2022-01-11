1c00c72ff540a646d559678fab5e04d5b2e9c75f
theumutt <97482778+theumutt@users.noreply.github.com>
2022-01-11 19:46:41 +0300

    Update and rename README.md to lua.lua

---
+++ lua.lua
@@ -0,0 +1,7 @@
+for i = 1, 10 do
+       game.Players.LocalPlayer.MaximumSimulationRadius = math.pow(math.huge,math.huge)*math.huge
+       game.Players.LocalPlayer.SimulationRadius = math.pow(math.huge,math.huge)*math.huge
+--any physics manipulation, teleporting, adding rocket propulsion, etc
+end
+
+NET Bypass for KRNL
